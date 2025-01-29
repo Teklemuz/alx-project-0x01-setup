f361c56b7eba181f310af34e49bb993b7fe28a4e
Teklemuz Ayenew <111185891+Teklemuz@users.noreply.github.com>
2025-01-29 14:08:38 +0300

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,14 +1,21 @@
-export interface PageRouteProps {
-    pageRoute: string;
-  }
-  
-  export interface ButtonProps {
-    buttonLabel: string;
-    buttonSize?: string;
-    buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
-    action?: () => void;
-  }
-  
-  export interface LayoutProps {
-    children: React.ReactNode;
-  }  
+export interface PageRouteProps { 
+  pageRoute: string;
+}
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+  action?: () => void;
+}
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface UserProps {
+  id: number;
+  username: string;
+  street: string;
+  catchPhrase: string;
+}
